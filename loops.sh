ee556afaa0334bd97158a00a7af60e25076464ad
upszot <upszot@gmail.com>
2023-10-26 22:55:34 -0300

    ejemplos de loops

--- loops.sh
+++ loops.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 
-LISTA=$1
-
-IFS=$'\n'
-for i in `cat $LISTA | awk '{print $1" "$2}'| grep -v ^#`
+for i in manzanas peras banana frutilla
 do
-	NOMBRE=$(echo  $i |awk '{print $1}')
-	APELLIDO=$(echo  $i |awk '{print $2}')
-	#echo "Linea: $i "
-	echo " apellido: $APELLIDO nombre: $NOMBRE"
+	echo "variable $i "
 
 done
